1eb107ffcddb0dfd72da0eb64157ec59cf8f12c8
SamBryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
2020-05-18 20:50:37 +0100

    function to get PAQR compatible name column

--- scripts/get_paqr_polya_bed.py
+++ scripts/get_paqr_polya_bed.py
@@ -3,6 +3,27 @@
 
 '''
 Script to get 'clusters' file for PAQR
+BED file from polyAsite website has following format:
+Chromosome | Start | End | <chr>:<coord>:<strand> | average tags PM | Strand | % samples supporting |
+n_supporting_protocols | average tags PM | cluster_annotation | upstream_polyA_signal
+
+Corresponding column names when importing BED with pyranges:
+['Chromosome', 'Start', 'End', 'Name', 'Score', 'Strand', 'ThickStart',
+       'ThickEnd', 'ItemRGB', 'BlockCount', 'BlockSizes'}
+
+PAQR's clusters file requires a 10 column BED file in following format:
+Chromosome | Start | End | <chr>:<strand>:<coord>:<cluster_annotation> | n_supporting_protocols/samples | Strand | site_n_on_exon |
+total_n_on_exon | <transcript_id>:<exon_number>:<n_exons_on_tr>:<exon_start>:<exon_end> | gene_id
+
+Column mapping:
+<chr>:<coord>:<strand> = Name
+average tags PM = Score
+% samples supporting = ThickStart
+n_supporting_protocols = ThickEnd
+average tags PM (2) = ItemRGB
+cluster_annotation = BlockCount
+upstream_polyA_signal = BlockSizes
+
 '''
 
 import pyranges as pyr
@@ -44,5 +65,27 @@ def join_by_intersect(pyranges1=None, pyranges2=None):
 
 
 polya_tr_df = join_by_intersect(pyranges1=polya_bed, pyranges2=tr_gtf)
-print(polya_tr_df)
+# print(polya_tr_df)
+# print(polya_tr_df.columns)
+
+
+def add_paqr_name_formatting(pyranges=None, col_name='paqr_name'):
+    '''
+    Add a column to pyranges consisting of
+    <chr>:<strand>:<coord>:<cluster_annotation>
+    '''
+    pyranges = pyranges.as_df()
+
+    def f(x): return x['Chromosome']+":" + x['Strand'] + \
+        ":" + x['Name'].split(':')[1] + ":" + x['BlockCount']
+    pyranges[col_name] = pyranges.apply(f, axis=1)
+
+    # pyranges[col_name] = pyranges['Chromosome'].astype(str)+":" + pyranges['Strand'].astype(
+    #    str) + ":" + split_by_colon(str=pyranges['Name'].astype(str), idx=1) + ":" + pyranges['BlockCount'].astype(str)
+
+    return pyr.PyRanges(pyranges)
+
+
+polya_tr_df = add_paqr_name_formatting(pyranges=polya_tr_df)
+print(polya_tr_df[['paqr_name']])
 print(polya_tr_df.columns)
