1863952512dd4f7aee9840a7778fd4154a9d279a
SamBryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
2020-06-05 12:44:57 +0100

    exploring effects of changing get_last_exons function

--- scripts/explore_provided_not_mine.py
+++ scripts/explore_provided_not_mine.py
@@ -10,10 +10,10 @@ What about the other 3.5k?
 '''
 
 import pyranges as pr
-#import pandas as pd
+import pandas as pd
 import os
-# going to modify get_non_overlapping_genes & get_multi_polya_transcripts functions slightly
-from get_compliant_genes import get_last_exons, filter_min_tsl, select_best_tsl_isoforms, remove_na_tsl
+# going to modify get_non_overlapping_genes, get_last_exons & get_multi_polya_transcripts functions slightly
+from get_compliant_genes import filter_min_tsl, select_best_tsl_isoforms, remove_na_tsl
 
 gtf = "/home/sam/paqr_annotations/gencode.vM14.annotation.gtf"
 polyA_bed = "/home/sam/paqr_annotations/mm10.PolyASite.v1.0.clusters.bed"
@@ -38,7 +38,30 @@ Changes to get_multi_polya_transcripts
 '''
 
 
-def get_non_overlapping_genes(gtf_df=None, subset_list=['protein_coding', 'lncRNA'], rm_na_tsl=True, gene_best_tsl=True, min_tsl=5, output_joined_df=False, output_filtered_df=False):
+def get_last_exons(ranges_obj, as_numeric=False):
+    '''
+    Returns last exon for each transcript
+    1. Select exons from Feature column
+    2. Group by transcript_id
+    3. Select last exon for each group (largest 'exon_number' value)
+    '''
+    df = ranges_obj.as_df()
+    df = df[df['Feature'] == 'exon']
+
+    if as_numeric is False:
+        # Select largest 'exon_number' row for each transcript_id - only solution I could get to work...
+        # https://stackoverflow.com/questions/15705630/get-the-rows-which-have-the-max-count-in-groups-using-groupby
+        # sort by exon number value - last exon = top of sort
+        # drop_duplicates keeps top row for each transcript_id - removes all rows but last exon
+        df = df.sort_values('exon_number', ascending=False).drop_duplicates(['transcript_id'])
+        return pr.PyRanges(df)
+    elif as_numeric is True:
+        df = df.sort_values(pd.to_numeric(
+            df['exon_number'], errors='coerce'), ascending=False).drop_duplicates(['transcript_id'])
+        return pr.PyRanges(df)
+
+
+def get_non_overlapping_genes(gtf_df=None, subset_list=['protein_coding', 'lncRNA'], rm_na_tsl=True, gene_best_tsl=True, min_tsl=5, output_joined_df=False, output_filtered_df=False, alt_last_function=False):
     '''
     returns list of transcript_ids of non-overlapping, protein-coding or lncRNA genes
     non-overlapping = last exon of transcript doesn't overlap with coordinates of a DIFFERENT gene
@@ -72,7 +95,12 @@ def get_non_overlapping_genes(gtf_df=None, subset_list=['protein_coding', 'lncRN
         len(set(exons.gene_id.to_list()))))
 
     # 1b. PyRanges object storing last exons for each transcript
-    exons = get_last_exons(ranges_obj=exons)
+    if alt_last_function is True:
+        exons = alt_get_last_exons(pyranges=exons)
+
+    elif alt_last_function is False:
+        exons = get_last_exons(ranges_obj=exons)
+
     print("terminal exons extracted for %s distinct genes" % (len(set(exons.gene_id.to_list()))))
     print("terminal exons extracted for %s distinct transcipts" %
           (len(set(exons.transcript_id.to_list()))))
@@ -149,7 +177,7 @@ def get_non_overlapping_genes(gtf_df=None, subset_list=['protein_coding', 'lncRN
     return transcript_id_list
 
 
-def get_multi_polya_transcripts(gtf_df=None, subset_list=None, polya_bed_path=None, polya_version=2, strand_specific=True, min_NOverlaps=2):
+def get_multi_polya_transcripts(gtf_df=None, subset_list=None, polya_bed_path=None, polya_version=2, strand_specific=True, min_NOverlaps=2, alt_last_function=False):
     '''
     Returns list of transcript ids with >=2 polyA_sites from polyA_bed_path overlapping last exon
     '''
@@ -171,7 +199,11 @@ def get_multi_polya_transcripts(gtf_df=None, subset_list=None, polya_bed_path=No
         polya_bed = pr.PyRanges(polya_bed)
 
     # print(polya_bed)
-    gtf_last_exons = get_last_exons(gtf_df)
+    if alt_last_function is True:
+        gtf_last_exons = alt_get_last_exons(gtf_df)
+    elif alt_last_function is False:
+        gtf_last_exons = get_last_exons(gtf_df)
+
     print("terminal exons extracted for %s distinct transcipts" %
           (len(set(gtf_last_exons.transcript_id.to_list()))))
 
@@ -253,6 +285,102 @@ gtf_pyranges = pr.readers.read_gtf(f=gtf,)
 # strip version numbers
 gtf_pyranges.transcript_id = gtf_pyranges.transcript_id.str.split('.', expand=True)[0]
 gtf_pyranges.gene_id = gtf_pyranges.gene_id.str.split('.', expand=True)[0]
+
+gtf_pyranges_provided = gtf_pyranges[gtf_pyranges.gene_id.isin(provided_not_mine_genes)]
+
+print(gtf_pyranges_provided[gtf_pyranges_provided.transcript_id == 'ENSMUST00000081551'].print(n=30))
+print(gtf_pyranges_provided.dtypes)
+# check get_last_exons_function - think it may be failing for transcripts with >= 10 exons
+
+
+def alt_get_last_exons(pyranges):
+    pyranges = pyranges.as_df()
+    pyranges = pyranges[pyranges.Feature == 'exon']
+
+    # set exon_number dtype to int32 (integer)
+    pyranges = pyranges.astype({'exon_number': 'int32'})
+
+    # for each transcript_id, select the largest exon number as entry to retain
+    idx = pyranges.groupby('transcript_id')['exon_number'].transform(
+        max) == pyranges['exon_number']
+    pyranges = pyranges[idx]
+    return pr.PyRanges(pyranges)
+
+
+existing_last_exons = get_last_exons(gtf_pyranges_provided)
+new_f_last_exons = alt_get_last_exons(gtf_pyranges_provided)
+
+print(existing_last_exons[['transcript_id', 'exon_number']])
+print(new_f_last_exons[['transcript_id', 'exon_number']])
+
+print(existing_last_exons[existing_last_exons.transcript_id == 'ENSMUST00000081551'].print())
+print(new_f_last_exons[new_f_last_exons.transcript_id == 'ENSMUST00000081551'].print())
+
+# print(existing_last_exons[existing_last_exons.transcript_id == 'ENSMUST00000081551'].print(n=30))
+
+#trs_to_check = ['ENSMUST00000193812','ENSMUST00000082908','ENSMUST00000192857','ENSMUST00000161581','ENSMUST00000183034','ENSMUST00000097786','ENSMUST00000187582','ENSMUST00000191048']
+#existing_to_print = existing_last_exons[existing_last_exons.transcript_id.isin(trs_to_check)]
+#new_f_to_print = new_f_last_exons[new_f_last_exons.transcript_id.isin(trs_to_check)]
+
+#print("existing function\n")
+# print(existing_to_print[['transcript_id','exon_number']])
+#print("new alternative function\n")
+# print(new_f_to_print[['transcript_id','exon_number']])
+
+# pandas to join the two dataframes by transcript_id
+
+
+def join_2_pyranges_pandas_like(x, y):
+    x = x.as_df()
+    y = y.as_df()
+
+    z = x.join(y.set_index('transcript_id'), on='transcript_id', rsuffix='_b')
+
+    return pr.PyRanges(z)
+
+
+existing_new_f_joined = join_2_pyranges_pandas_like(existing_last_exons, new_f_last_exons)
+
+# get cases where exon numbers do not match up
+existing_new_f_joined = existing_new_f_joined.subset(lambda df: pd.to_numeric(
+    df.exon_number, errors='coerce') != pd.to_numeric(df.exon_number_b, errors='coerce'))
+
+print(existing_new_f_joined[['transcript_id', 'exon_number', 'Start_b', 'End_b', 'Strand_b', 'exon_number_b']].print(n=20))
+
+
+# PROBLEM IS BECAUSE GET LAST EXONS WAS SELECTING MATHCING VALUE BASED ON OBJECT DATATYPE NOT AN INTEGER DATATYPE
+# if transcript had >= 10 exons - exon 9 was selected :(
+
+# function check - does drop_duplicates approach work if I convert to numeric first?
+#convert_numeric_existing_last_exons = get_last_exons(gtf_pyranges_provided,as_numeric=True)
+#print("checking existing function if convert to numeric inside it\n")
+# print(convert_numeric_existing_last_exons[['transcript_id','exon_number']])
+
+
+# Run polyA overlap on provided_not_mine_transcripts using existing and alternative get_last_exons function
+print("--------\nchecking for provided_not_mine last exon poly(A) overlap if use existing get_last_exons function\n")
+
+provided_not_mine_transcripts = get_trs_for_gene_list(
+    pyranges=gtf_pyranges, gene_list=provided_not_mine_genes)
+
+get_multi_polya_transcripts(
+    gtf_df=gtf_pyranges, subset_list=provided_not_mine_transcripts, polya_bed_path=polyA_bed, polya_version=1)
+
+
+print("--------\nchecking for provided_not_mine last exon poly(A) overlap if use alternative get_last_exons function\n")
+get_multi_polya_transcripts(
+    gtf_df=gtf_pyranges, subset_list=provided_not_mine_transcripts, polya_bed_path=polyA_bed, polya_version=1, alt_last_function=True)
+
+# check how many would not overlap with other genes on the same strand
+print("--------\ncheck how many with updated last exons function would be valid with my worklow (non-overlapping with a different gene)\n")
+
+provided_not_mine_valid_alt_lastExons_f_transcripts = get_non_overlapping_genes(
+    gtf_df=gtf_pyranges, subset_list=provided_not_mine_genes, rm_na_tsl=False, alt_last_function=True)
+
+provided_not_mine_valid_alt_lastExons_f_multi_polya_transcripts = get_multi_polya_transcripts(
+    gtf_df=gtf_pyranges, polya_bed_path=polyA_bed, polya_version=1, subset_list=provided_not_mine_valid_alt_lastExons_f_transcripts, alt_last_function=True)
+
+'''
 
 # 1. DO THE GENES IN PROVIDED ANNOTATIONS BUT NOT MY WORKFLOW HAVE 'protein_coding' or 'lncRNA' gene_type tags?
 print("\nAssessing whether provided_not_mine genes lack a 'protein_coding' or 'lncRNA' gene_type tag\n")
@@ -359,50 +487,50 @@ print("\n---------------\nCHECKING HOW MANY GENES AND TRANSCRIPTS NEVER HAVE OVE
 provided_not_mine_never_any_multi_polyA_transcripts = [
     transcript for transcript in provided_not_mine_transcripts if transcript not in ignore_strand_nOverlaps_1_multi_polya_provided_not_mine_transcripts]
 
-# print(len(provided_not_mine_never_any_multi_polyA_transcripts))
+#print(len(provided_not_mine_never_any_multi_polyA_transcripts))
 print("number of transcripts belonging to genes in provided_not_mine_gene_list that do not have overlapping poly(A) sites regardless of above searches is %s" %
       (len(set(provided_not_mine_never_any_multi_polyA_transcripts))))
 
-print("(sanity check) - number of transcripts in 'gtf_pyranges_provided' is %s" %
-      (len(set(gtf_pyranges_provided.transcript_id.to_list()))))
+print("(sanity check) - number of transcripts in 'gtf_pyranges_provided' is %s" % (len(set(gtf_pyranges_provided.transcript_id.to_list()))))
 
-# Get gene_ids corresponding to transcripts in ignore_strand_nOverlaps_1_multi_polya_provided_not_mine_transcripts
-provided_not_mine_any_multi_polyA_genes = gtf_pyranges_provided[gtf_pyranges_provided.transcript_id.isin(
-    ignore_strand_nOverlaps_1_multi_polya_provided_not_mine_transcripts)].gene_id.to_list()
+#Get gene_ids corresponding to transcripts in ignore_strand_nOverlaps_1_multi_polya_provided_not_mine_transcripts
+provided_not_mine_any_multi_polyA_genes = gtf_pyranges_provided[gtf_pyranges_provided.transcript_id.isin(ignore_strand_nOverlaps_1_multi_polya_provided_not_mine_transcripts)].gene_id.to_list()
 
-print("number of genes corresponding to transcripts with multiple poly(A) sites by any parameters is %s" %
-      (len(set(provided_not_mine_any_multi_polyA_genes))))
+print("number of genes corresponding to transcripts with multiple poly(A) sites by any parameters is %s" % (len(set(provided_not_mine_any_multi_polyA_genes))))
 
-# get list of gene_ids in which never any overlap (i.e. not in provided_not_mine_never_any_multi_polyA_genes)
-provided_not_mine_never_any_multi_polyA_genes = list(set(
-    gtf_pyranges_provided[~gtf_pyranges_provided.gene_id.isin(provided_not_mine_any_multi_polyA_genes)].gene_id.to_list()))
+#get list of gene_ids in which never any overlap (i.e. not in provided_not_mine_never_any_multi_polyA_genes)
+provided_not_mine_never_any_multi_polyA_genes = list(set(gtf_pyranges_provided[~gtf_pyranges_provided.gene_id.isin(provided_not_mine_any_multi_polyA_genes)].gene_id.to_list()))
 
-print("(sanity check) number of genes in which none of its transcripts ever have multi_polyA transcripts (any parameters) is %s " %
-      (len(set(provided_not_mine_never_any_multi_polyA_genes))))
+print("(sanity check) number of genes in which none of its transcripts ever have multi_polyA transcripts (any parameters) is %s " % (len(set(provided_not_mine_never_any_multi_polyA_genes))))
 
-# write never any multiple polyA_genes to file
-with open(os.path.join(output_dir, "provided_not_mine_never_any_multi_polyA_tr_gene_ids.txt"), "w") as outfile:
+#write never any multiple polyA_genes to file
+with open(os.path.join(output_dir,"provided_not_mine_never_any_multi_polyA_tr_gene_ids.txt"),"w") as outfile:
     for gene in provided_not_mine_never_any_multi_polyA_genes:
         outfile.write("%s\n" % gene)
 
-# write never any multiple polyA transcripts to file
-with open(os.path.join(output_dir, "provided_not_mine_never_any_multi_polyA_transcript_ids.txt"), "w") as outfile:
+#write never any multiple polyA transcripts to file
+with open(os.path.join(output_dir,"provided_not_mine_never_any_multi_polyA_transcript_ids.txt"),"w") as outfile:
     for transcript in provided_not_mine_never_any_multi_polyA_transcripts:
         outfile.write("%s\n" % transcript)
 
 
-# Write GTF containing all never_any_multi_polyA_transcripts to file
-gtf_pyranges_provided = gtf_pyranges_provided[gtf_pyranges_provided.transcript_id.isin(
-    provided_not_mine_never_any_multi_polyA_transcripts)]
-gtf_pyranges_provided.to_gtf(path=os.path.join(
-    output_dir, "gencode.mm10.vM14.atlas1.provided.not.mine.never.any.multi.polyA.transcripts.gtf"))
+#Write GTF containing all never_any_multi_polyA_transcripts to file
+gtf_pyranges_provided = gtf_pyranges_provided[gtf_pyranges_provided.transcript_id.isin(provided_not_mine_never_any_multi_polyA_transcripts)]
+gtf_pyranges_provided.to_gtf(path=os.path.join(output_dir,"gencode.mm10.vM14.atlas1.provided.not.mine.never.any.multi.polyA.transcripts.gtf"))
+
+#write GTF containing terminal exons of all_never_any_multi_polyA_transcripts to file
+gtf_pyranges_provided = get_last_exons(ranges_obj=gtf_pyranges_provided)
+print("(sanity check) - number of terminal exons of never_any_multi_polyA_transcripts is %s (should = number of transcripts)" % (len(set(gtf_pyranges_provided.transcript_id.to_list()))))
+
+gtf_pyranges_provided.to_gtf(path=os.path.join(output_dir,"gencode.mm10.vM14.atlas1.provided.not.mine.never.any.multi.polyA.Texons.gtf"))
 
 #test = gtf_pyranges_provided[gtf_pyranges_provided.Feature == 'transcript']
-# test = test[~test.transcript_id.isin(
+#test = test[~test.transcript_id.isin(
 #    ignore_strand_nOverlaps_1_multi_polya_provided_not_mine_transcripts)]
 
 #print("n genes in test is %s" % len(set(test.gene_id.to_list())))
-# provided_not_mine_never_any_multi_polyA_genes = gtf_pyranges_provided[gtf_pyranges_provided.transcript_id.isin(
+#provided_not_mine_never_any_multi_polyA_genes = gtf_pyranges_provided[gtf_pyranges_provided.transcript_id.isin(
 #    provided_not_mine_never_any_multi_polyA_transcripts)].gene_id.to_list()
 
 # print(provided_not_mine_never_any_multi_polyA_transcripts)
+'''
